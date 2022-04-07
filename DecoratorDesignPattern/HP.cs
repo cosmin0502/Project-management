9f93ea4e1f133e8b3d18d8aa79f0f73b23f28a6e
cosmin0502 <66167596+cosmin0502@users.noreply.github.com>
2022-04-07 13:17:08 +0300

    Add files via upload

---
+++ DecoratorDesignPattern/HP.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;                                      
+
+namespace DecoratorMementoDesignPattern
+{
+    //Memento Desing Patterns
+    public class HP
+    {
+        public string Size { get; set; }
+        public string Price { get; set; }
+        public string Memory { get; set; }
+        public string CameraFront { get; set; }      
+
+        public HP(string Size, string Price, String Memory, String CameraFront)
+        {
+            this.Size = Size;
+            this.Price = Price;
+            this.Memory = Memory;
+            this.CameraFront = CameraFront;         
+        }
+
+        public string GetDetails()
+        {
+            return "Samsung [Size= " + Size + ", Price= " + Price + ", Memory= " + Memory + "," +" CameraFront= " + CameraFront + "] ";
+        }
+    }
+}
---
+++ DecoratorDesignPattern/NumberLaptopModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;                               
+
+namespace DecoratorMementoDesignPattern
+{
+    public class NumberLaptopModels
+    {
+        public int AddNumberLaptopModels(int NumberModels1, int NumberModels2)
+        {
+            return NumberModels1 + NumberModels2;
+        }
+    }
+}
