aa8d7757b5383b93c3f6bb043ed2f881f78fbff5
cosmin0502 <66167596+cosmin0502@users.noreply.github.com>
2022-04-07 13:21:57 +0300

    Add files via upload

---
+++ DecoratorDesignPattern/Caretaker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;                          
+
+namespace DecoratorMementoDesignPattern
+{
+    //Memento Desing Patterns
+
+    public class Caretaker
+    {
+        private List<Memento> hplist = new List<Memento>();
+        public void AddMemento(Memento m)
+        {
+            hplist.Add(m);
+            Console.WriteLine("HP is snapshots maintained by caretaker: " + m.GetDetails());
+        }
+        public Memento GetMemento(int index)
+        {
+            return hplist[index];
+        }
+    }
+}
---
+++ DecoratorDesignPattern/LaptopDecorator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;                    
+
+namespace DecoratorMementoDesignPattern
+{
+    // DecoratorDesignPattern
+    public abstract class LaptopDecorator : ILaptop
+    {
+        protected ILaptop Laptop;
+        public LaptopDecorator(ILaptop Laptop)
+        {
+            this.Laptop = Laptop;
+        }
+
+        public virtual ILaptop ComponentsLaptop()
+        {
+            return Laptop.ComponentsLaptop();
+        }
+
+    }
+}
